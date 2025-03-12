bf5b06a6c59121a3ad38070aad86b5c61e9535f1
ip-repo <123945379+ip-repo@users.noreply.github.com>
2025-03-12 21:21:44 +0200

    Update set_model.py

--- source/pyfiles/set_model.py
+++ source/pyfiles/set_model.py
@@ -5,7 +5,7 @@ from list_model_exe_path import list_models_path
 def set_model() -> None:
     """
     Reads available models, filters out not allowed models, and prompts the user to select a model.
-        - Retrieves the list of models using the `list_models_path` command.
+        - Retrieves the list of models using the `list_models_path` variable.
         - Filters out disallowed models specified in a "not_allowed.txt" file located in the "info" folder.
         - Presents the allowed models and provides an option for the user to select one.
         - Saves the chosen model to a "model.txt" file in the "info" folder.
@@ -18,12 +18,19 @@ def set_model() -> None:
 
     Notes:
         - If no command line argumnets then the default option is selected (index 0).
-        - Handles invalid user input gracefully by defaulting to a safe option.
     """
+    
     argv = sys.argv
     models = os.popen(list_models_path)
-    info_folder = "\\\\".join(list_models_path.split("\\")[:-2]) + "\\\\info\\\\"
-    not_allowed_models = [str(nmodel) for nmodel in open(f"{info_folder}not_allowed.txt","r").read().strip().split("\n")]
+    
+    
+    parent_folder = os.path.dirname(os.path.dirname(list_models_path))
+
+   
+    info_folder = os.path.join(parent_folder, "info")
+
+  
+    not_allowed_models = [str(nmodel) for nmodel in open(os.path.join(info_folder,"not_allowed.txt"),"r").read().strip().split("\n")]
     
     allowed_models = []
     for line in models.read().strip().split("\n"):
@@ -52,8 +59,10 @@ def set_model() -> None:
             choose = 0
     except Exception:
         choose = d.keys()[0]
-    with open(f"{info_folder}model.txt", "w") as file:
+    
+    with open(os.path.join(info_folder,"model.txt"), "w") as file:
         file.write(allowed_models[choose])
 
+   
 if __name__ == "__main__":
     set_model()
